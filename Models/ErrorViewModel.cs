912531a3e7cdb03441b5e210eb82c21ea3992ce1
Chet Husk <chusk3@gmail.com>
2024-10-20 14:39:40 -0500

    initial repo layout with no minification or bundling

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace build_analysis.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
