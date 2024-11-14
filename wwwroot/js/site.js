5da87a1d48ff028bd94b462839777aaef42e54e2
Rainer Sigwald <raines@microsoft.com>
2024-11-14 11:16:34 +0100

    Add some JavaScript

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,17 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿// Copilot, can you add a javascript function that makes the page background a
+// gradient and rotates it around slowly?
 
-// Write your JavaScript code.
+function createGradientBackground() {
+    let angle = 0;
+    const body = document.body;
+
+    function updateBackground() {
+        angle = (angle + 1) % 360;
+        body.style.background = `linear-gradient(${angle}deg, #ff7e5f, #feb47b)`;
+        requestAnimationFrame(updateBackground);
+    }
+
+    updateBackground();
+}
+
+createGradientBackground();
